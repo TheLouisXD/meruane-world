f35977acafeb3caf9bb7211d14b357beef23f0bc
TheLouisXD <106026658+TheLouisXD@users.noreply.github.com>
2024-12-05 17:51:15 -0300

    ui de monedas arreglado

--- scripts/moneda.gd
+++ scripts/moneda.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var ui: CanvasLayer = %UI
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
 		queue_free()
-		game_manager.add_point()
+		ui.add_point()
